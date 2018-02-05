7af71848d7ce11504ff9a2bb7230a57b1526464c
iga <iga.smarz@gmail.com>
2018-02-05 15:02:24 +0100

    initial commit

---
+++ chapter02/src/main/java/chapter02/hibernate/Message.java
@@ -0,0 +1,48 @@
+package chapter02.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  Long id;
+  @Column(nullable = false)
+  String text;
+
+  public Message() {
+  }
+
+  public Message(String text) {
+    this.text = text;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  @Override
+  public String toString() {
+    return "Message{" +
+        "id=" + getId() +
+        ", text='" + getText() + '\'' +
+        '}';
+  }
+}
